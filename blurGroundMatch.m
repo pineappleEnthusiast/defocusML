f3b8a7d9af283cbaea7c5d010838b810e4b6dcb6
pineappleEnthusiast <saloni.a.modi@gmail.com>
2020-07-03 13:17:33 -0500

    quicc update
    added new data reorganized project structure

---
+++ blurGroundMatch.m
@@ -0,0 +1,8 @@
+%ground to blur feature matching
+
+%define region of interest on blur 
+%crop region of interest on blur
+%estimate rotation angle of region imaged
+%try template matching 
+%rotate full ground same angles as B
+%try template matching again
