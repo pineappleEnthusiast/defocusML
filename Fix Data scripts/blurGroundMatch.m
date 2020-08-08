fb352470b1444ff12b74f3631a9d99b31609c4d2
pineappleEnthusiast <saloni.a.modi@gmail.com>
2020-08-08 15:43:34 -0500

    fixing bam data
    matched rotations, tried to get rid of uneven illumination

--- Fix Data scripts/blurGroundMatch.m
+++ Fix Data scripts/blurGroundMatch.m
@@ -1,8 +1,68 @@
-%ground to blur feature matching
+%this is for data from BAM
+% pathtest = "C:\Users\kanga\Documents\MATLAB\project\defocusML\Data\Raw Data from BAM\blurred rotated 1";
+% a = imread(strcat(pathtest, "\20200608_155700.000.jpg"));
 
-%define region of interest on blur 
-%crop region of interest on blur
-%estimate rotation angle of region imaged
-%try template matching 
-%rotate full ground same angles as B
-%try template matching again
+%get crop shape
+% imshow(a)
+% impixelinfo
+% acrop = drawrectangle();
+% position = customWait(acrop);
+% 
+% %crop all blurred to square
+% testfiles = dir (strcat(pathtest,'/*.jpg'));
+% Ltest = length (testfiles);
+% pathtestprime = 'C:\Users\kanga\Documents\MATLAB\project\TEMP';
+% for i=1:Ltest
+%    testimage{i}=imread(strcat(pathtest,'/',testfiles(i).name));  
+%    testA = testimage{i};
+%    testAPrime = imcrop(testA, position);
+%    imwrite(testAPrime, strcat(pathtestprime,'/', int2str(i),'.jpg'), 'jpg');   
+% end
+% 
+pathtest = "C:\Users\kanga\Documents\MATLAB\project\TEMP";
+test = imread(strcat(pathtest, "\1.jpg"));
+ground = imread("C:\Users\kanga\Documents\MATLAB\project\defocusML\Data\Raw Data from BAM\ground 1\DSC_0001.jpg");
+ground=imrotate(ground,90);
+degree = -4.9315;
+
+imshow(ground)
+impixelinfo
+groundcrop = drawrectangle();
+position = customWait(groundcrop);
+
+init = imcrop(ground,position);
+pathtestprime = 'C:\Users\kanga\Documents\MATLAB\project\defocusML\Data\Fixed Data from BAM\ground';
+imwrite(ground, strcat(pathtestprime,'/', '2','.jpg'), 'jpg');   
+
+groundrot1 = imrotate(init,abs(degree),"crop");
+result1 = imcrop(init, position);
+imwrite(groundrot1, strcat(pathtestprime,'/', '1','.jpg'), 'jpg');   
+
+%groundrot = init;
+
+for i=3:278
+   groundrot = imrotate(ground,degree*(i-1),'crop');
+   result = imcrop(groundrot, position);
+   imwrite(groundrot, strcat(pathtestprime,'/', int2str(i),'.jpg'), 'jpg');   
+end
+
+function pos = customWait(hROI)
+% Listen for mouse clicks on the ROI
+l = addlistener(hROI,'ROIClicked',@clickCallback);
+% Block program execution
+uiwait;
+% Remove listener
+delete(l);
+% Return the current position
+pos = hROI.Position;
+end
+
+function clickCallback(~,evt)
+if strcmp(evt.SelectionType,'double')
+    uiresume;
+end
+end
+
+%template match ground to each incoherent
+
+
--- Fix Data scripts/templatematching.m
+++ Fix Data scripts/templatematching.m
@@ -1,47 +1,56 @@
-    ground = imread('F:/GROUND/000121.jpg');
-    test = imread('F:/TESTprime/000121.jpg');
+pathtest = "C:\Users\kanga\Documents\MATLAB\project\TEMP";
+test = imread(strcat(pathtest, "\57.jpg"));
 
-    A1 = ground;
+pathground = "C:\Users\kanga\Documents\MATLAB\project\defocusML\Data\Fixed Data from BAM\ground";
+ground = imread(strcat(pathground,"\57.jpg"));
 
-    %Read the Target Image
-    B1 = test;
+test = rgb2gray(test);
+ground = rgb2gray(ground);
 
-    A = A1(:,:,1);
-    B = B1(:,:,1);
+A1 = ground;
 
+%Read the Target Image
+B1 = test;
 
-    normx_corrmap=normxcorr2(B(:,:),A(:,:));
-
-    imshow(normx_corrmap)
-    
-    maxptx = max(normx_corrmap(:));
-    
-    [x1,y1]=find(normx_corrmap==maxptx);
-    
-    if(x1>size(A1,1))
-        x1=size(A1,1);
-    end
-    
-    if(y1>size(A1,2))
-        y1=size(A1,2);
-    end
-   
-    c = x1-size(B,1);
-    if(c<1)
-        c=1;
-    end
-    
-    d = y1-size(B,2);
-    if(d<1)
-        d=1;
-    end     
-        
-    groundcropped = A1(c:x1,d:y1);
+A = A1(:,:,1);
+B = B1(:,:,1);
 
 
+normx_corrmap=normxcorr2(B(:,:),A(:,:));
+
+imshow(normx_corrmap)
+
+maxptx = max(normx_corrmap(:));
+
+[x1,y1]=find(normx_corrmap==maxptx);
+
+if(x1>size(A1,1))
+    x1=size(A1,1);
+end
+
+if(y1>size(A1,2))
+    y1=size(A1,2);
+end
+
+c = x1-size(B,1);
+if(c<1)
+    c=1;
+end
+
+d = y1-size(B,2);
+if(d<1)
+    d=1;
+end     
+
+groundcropped = A1(c:x1,d:y1);
+figure,
+subplot(1,3,1); imshow(test);
+subplot(1,3,2); imshow(ground);
+subplot(1,3,3); imshow(groundcropped);
 
 
-    
-    
 
-    
+
+
+
+
